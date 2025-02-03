e47f20f8b4d78853d8a5fdc92d06555b403be1b4
Jesse Piccione <jessepiccione@yahoo.com>
2025-02-03 22:50:00 +0000

    built a manual representation of next js create-next-app to gain an understanding of the differences between react and other jobs

---
+++ src/app/layout.js
@@ -0,0 +1,9 @@
+export default function RootLayout(props){
+    return (
+        <html lang='en'>
+            <body>
+                {props.children}
+            </body>
+        </html>
+    )
+}
