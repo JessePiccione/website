cc0e43d9e31cb1393c761133ec6bf6858a5e602d
Jesse Piccione <jessepiccione@gmail.com>
2025-02-23 22:05:20 -0500

    added boiler plate css and finished structure of the html document for today next up implementing more css and finally functionality

--- src/components/postsmall.js
+++ src/components/postsmall.js
@@ -1,3 +1,5 @@
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
 export default function Post({technologies, title, description, URL}){
     return (
         <section className='blog-post'>
@@ -5,6 +7,12 @@ export default function Post({technologies, title, description, URL}){
                 <iframe title={title} src={URL}></iframe>
                 <h3>{title}</h3>
                 <p>{description}</p>
+                <p>
+                    <button>
+                        See More
+                        <FontAwesomeIcon icon={faArrowRight} />
+                    </button>
+                </p>
             </article>
         </section>
     )
