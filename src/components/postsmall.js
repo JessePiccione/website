8a2b21182b2edb47c080e38472b0432399de9509
Jesse Piccione <jessepiccione@yahoo.com>
2025-03-24 21:39:54 -0400

    adjusted settings and code to follow style guide better began trying out style guide formats for blog posts.

--- src/components/postsmall.js
+++ src/components/postsmall.js
@@ -1,18 +1,38 @@
+"use client"
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import { faAngleDown, faAngleUp } from '@fortawesome/free-solid-svg-icons'
+import {useReducer} from 'react'
+
+export function toggleSizeState(previousState, args){
+    switch (previousState){
+        case 'small':
+            return 'big'
+        case 'big':
+            return 'small'
+        default:
+            return 'small'
+    }
+}
 export default function Post({technologies, title, description, URL}){
+    const [postSize, togglePostSize] = useReducer(toggleSizeState, toggleSizeState())
+    const handleClick = (e) => {
+        e.preventDefault()
+        togglePostSize()
+    }
     return (
-        <section className='blog-post'>
+        <section className={`blog-post ${postSize}`}>
             <article>
                 <iframe title={title} src={URL}></iframe>
-                <h3>{title}</h3>
-                <p>{description}</p>
-                <p>
-                    <button>
-                        See More
-                        <FontAwesomeIcon icon={faArrowRight} />
-                    </button>
-                </p>
+                <section>
+                    <h3>{title}</h3>
+                    <p>{description}</p>
+                    <p>
+                        <button onClick={handleClick}>
+                            {(postSize==='big')?'See More':'See Less'}
+                            <FontAwesomeIcon className='fontAwesomeIcon' icon={(postSize==='small')?faAngleDown:faAngleUp} />
+                        </button>
+                    </p>
+                </section>
             </article>
         </section>
     )
