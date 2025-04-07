f8cc3ca9ca6d42e3468f22f09a9389acc67a8936
Jesse Piccione <jessepiccione@gmail.com>
2025-04-06 21:25:14 -0400

    redeploying fixed meta data to site

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,41 +1,39 @@
 import '@fontsource-variable/roboto-serif';
 import '@fontsource-variable/work-sans';
+export const metadata = {
+    title: 'Piccione Software Development',
+    description: 'Jesse Piccione Development (piccione.dev) is the professional portfolio of Jesse Piccione, a skilled software engineer specializing in full-stack web development. This site showcases expertise in modern technologies such as Python, Django, JavaScript, and cloud platforms like Google Cloud. Featuring a dynamic e-resume, project highlights, and technical insights, piccione.dev demonstrates Jesse’s ability to craft innovative, user-focused solutions. Explore a blend of creativity and technical proficiency designed to connect with clients, employers, and fellow developers.',
+    keywords: 'software development, web development, full-stack development, python, django, javascript, google cloud, portfolio',
+    author: 'Jesse Piccione',
+    robots: 'index, follow',
+    googlebot: 'index, follow',
+    google: 'notranslate',
+    handheldFriendly: 'True',
+    applicationName: 'Piccione Development',
+    appleMobileWebAppTitle: 'Piccione Development',
+    appleMobileWebAppCapable: 'yes',
+    revised: '2025-04-06',
+    rating: 'safe for kids',
+    distribution: 'global',
+    language: 'English',
+    url: 'https://piccione.dev',
+    canonical: 'https://piccione.dev',
+    ogTitle: 'Piccione Software Development',
+    ogType: 'website',
+    ogUrl: 'https://piccione.dev',
+    ogImage: 'https://piccione.dev/opengraph-image.png',
+    ogDescription: 'Jesse Piccione Development (piccione.dev) is the professional portfolio of Jesse Piccione, a skilled software engineer specializing in full-stack web development. This site showcases expertise in modern technologies such as Python, Django, JavaScript, and cloud platforms like Google Cloud. Featuring a dynamic e-resume, project highlights, and technical insights, piccione.dev demonstrates Jesse’s ability to craft innovative, user-focused solutions. Explore a blend of creativity and technical proficiency designed to connect with clients, employers, and fellow developers.',
+    favicon: '/favicon.ico',
+    appleTouchIcon: '/apple-touch-icon.png',
+    icon: '/favicon.ico',
+    iconSizes: '48x48',
+    appleTouchIconSizes: '180x180',
+    appleTouchIconType: 'image/png',
+    appleTouchIconRel: 'apple-touch-icon',
+}
 export default function RootLayout(props){
     return (
         <html lang='en'>
-            <head>
-                <title>Piccione Software Development</title>
-                
-                <meta name='title' content='Piccione Software Development'/>
-                <meta name='description' content="Jesse Piccione Development (piccione.dev) is the professional portfolio of Jesse Piccione, a skilled software engineer specializing in full-stack web development. This site showcases expertise in modern technologies such as Python, Django, JavaScript, and cloud platforms like Google Cloud. Featuring a dynamic e-resume, project highlights, and technical insights, piccione.dev demonstrates Jesse’s ability to craft innovative, user-focused solutions. Explore a blend of creativity and technical proficiency designed to connect with clients, employers, and fellow developers."/>
-                <meta charSet='UTF-8' />
-                <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-                <meta name='robots' content='index, follow' />
-                <meta name='googlebot' content='index, follow' />
-                <meta name='google' content='notranslate' />
-                <meta name='HandheldFriendly' content='True' />
-                <meta name='author' content='Jesse Piccione' />
-                <meta name='keywords' content='software development, web development, full-stack development, python, django, javascript, google cloud, portfolio' />
-                <meta name='theme-color' content='#000000' />
-                <meta name='application-name' content='Piccione Development' />
-                <meta name='apple-mobile-web-app-title' content='Piccione Development' />
-                <meta name='apple-mobile-web-app-capable' content='yes' />
-                <meta name='revised' content='2025-04-06' />
-                <meta name='rating' content='safe for kids' />
-                <meta name='distribution' content='global' />
-                <meta name='language' content='English' />
-                <meta name='url' content='https://piccione.dev' />
-                <meta name='canonical' content='https://piccione.dev' />
-
-                <meta property='og:title' content='Piccione Software Development'/>
-                <meta property='og:type' content='website'/>
-                <meta property='og:url' content='https://piccione.dev' />
-                <meta property='og:image' content='https:/piccione.dev/opengraph-image.png'/>
-                <meta property='og:description' content="Jesse Piccione Development (piccione.dev) is the professional portfolio of Jesse Piccione, a skilled software engineer specializing in full-stack web development. This site showcases expertise in modern technologies such as Python, Django, JavaScript, and cloud platforms like Google Cloud. Featuring a dynamic e-resume, project highlights, and technical insights, piccione.dev demonstrates Jesse’s ability to craft innovative, user-focused solutions. Explore a blend of creativity and technical proficiency designed to connect with clients, employers, and fellow developers."/>
-                
-                <link rel='icon' href='/favicon.ico' sizes='48x48' />
-                <link rel='apple-touch-icon' href='/apple-touch-icon.png' />
-            </head>
             <body>
                 {props.children}
             </body>
